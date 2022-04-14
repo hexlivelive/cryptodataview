65fe713e5780af81eec3b3867d2670cb703f0656
sajadevo <nawabisajadahmad@gmail.com>
2022-04-15 02:35:35 +0430

    Prepare the table

---
+++ src/layouts/dashboards/me/data.ts
@@ -0,0 +1,802 @@
+export const data = [
+  {
+    id: "1",
+    name: "Ariana Solomon",
+    lastPrice: "292",
+    "24hChange": "0.7557883855",
+    marketCap: "350",
+    "24hVolume": "202"
+  },
+  {
+    id: "2",
+    name: "Hakeem Perry",
+    lastPrice: "364",
+    "24hChange": "1.1850142413",
+    marketCap: "418",
+    "24hVolume": "464"
+  },
+  {
+    id: "3",
+    name: "Aspen Holt",
+    lastPrice: "92",
+    "24hChange": "1.2365276569",
+    marketCap: "183",
+    "24hVolume": "144"
+  },
+  {
+    id: "4",
+    name: "Alexander Langley",
+    lastPrice: "433",
+    "24hChange": "0.6157484591",
+    marketCap: "145",
+    "24hVolume": "337"
+  },
+  {
+    id: "5",
+    name: "Rajah Black",
+    lastPrice: "339",
+    "24hChange": "1.0722932085",
+    marketCap: "467",
+    "24hVolume": "411"
+  },
+  {
+    id: "6",
+    name: "Rhonda Mayo",
+    lastPrice: "97",
+    "24hChange": "1.0908416371",
+    marketCap: "111",
+    "24hVolume": "457"
+  },
+  {
+    id: "7",
+    name: "Mufutau Pruitt",
+    lastPrice: "18",
+    "24hChange": "1.1033218782",
+    marketCap: "318",
+    "24hVolume": "332"
+  },
+  {
+    id: "8",
+    name: "Cherokee Thomas",
+    lastPrice: "77",
+    "24hChange": "1.3661120374",
+    marketCap: "241",
+    "24hVolume": "39"
+  },
+  {
+    id: "9",
+    name: "Pandora Decker",
+    lastPrice: "243",
+    "24hChange": "1.2705676064",
+    marketCap: "309",
+    "24hVolume": "208"
+  },
+  {
+    id: "10",
+    name: "Adam Rhodes",
+    lastPrice: "446",
+    "24hChange": "1.0706311171",
+    marketCap: "229",
+    "24hVolume": "498"
+  },
+  {
+    id: "11",
+    name: "Dante Leon",
+    lastPrice: "245",
+    "24hChange": "1.0319083187",
+    marketCap: "150",
+    "24hVolume": "261"
+  },
+  {
+    id: "12",
+    name: "Lara Lowe",
+    lastPrice: "492",
+    "24hChange": "1.0077344761",
+    marketCap: "410",
+    "24hVolume": "396"
+  },
+  {
+    id: "13",
+    name: "Madonna Chambers",
+    lastPrice: "475",
+    "24hChange": "0.7789020438",
+    marketCap: "495",
+    "24hVolume": "328"
+  },
+  {
+    id: "14",
+    name: "Chaney Tate",
+    lastPrice: "282",
+    "24hChange": "1.1796589433",
+    marketCap: "472",
+    "24hVolume": "277"
+  },
+  {
+    id: "15",
+    name: "Lamar Luna",
+    lastPrice: "394",
+    "24hChange": "1.1223498842",
+    marketCap: "230",
+    "24hVolume": "195"
+  },
+  {
+    id: "16",
+    name: "Derek French",
+    lastPrice: "275",
+    "24hChange": "0.9609465124",
+    marketCap: "141",
+    "24hVolume": "96"
+  },
+  {
+    id: "17",
+    name: "Blossom Kline",
+    lastPrice: "116",
+    "24hChange": "1.1519123748",
+    marketCap: "57",
+    "24hVolume": "21"
+  },
+  {
+    id: "18",
+    name: "Leilani Buchanan",
+    lastPrice: "429",
+    "24hChange": "1.4736228646",
+    marketCap: "116",
+    "24hVolume": "358"
+  },
+  {
+    id: "19",
+    name: "Jack Chen",
+    lastPrice: "169",
+    "24hChange": "1.1687753864",
+    marketCap: "429",
+    "24hVolume": "60"
+  },
+  {
+    id: "20",
+    name: "Zeph Lancaster",
+    lastPrice: "461",
+    "24hChange": "0.7697628781",
+    marketCap: "105",
+    "24hVolume": "74"
+  },
+  {
+    id: "21",
+    name: "Peter Vaughan",
+    lastPrice: "383",
+    "24hChange": "0.6819371522",
+    marketCap: "497",
+    "24hVolume": "126"
+  },
+  {
+    id: "22",
+    name: "Jackson Roberts",
+    lastPrice: "137",
+    "24hChange": "0.8224291206",
+    marketCap: "295",
+    "24hVolume": "323"
+  },
+  {
+    id: "23",
+    name: "Noble Murray",
+    lastPrice: "25",
+    "24hChange": "0.6432328921",
+    marketCap: "479",
+    "24hVolume": "488"
+  },
+  {
+    id: "24",
+    name: "Ulric Rivers",
+    lastPrice: "268",
+    "24hChange": "1.4502281372",
+    marketCap: "135",
+    "24hVolume": "190"
+  },
+  {
+    id: "25",
+    name: "Otto Ellis",
+    lastPrice: "151",
+    "24hChange": "0.7148376154",
+    marketCap: "106",
+    "24hVolume": "324"
+  },
+  {
+    id: "26",
+    name: "Winter Irwin",
+    lastPrice: "236",
+    "24hChange": "0.7887866893",
+    marketCap: "237",
+    "24hVolume": "235"
+  },
+  {
+    id: "27",
+    name: "Althea Mendez",
+    lastPrice: "140",
+    "24hChange": "0.7034461188",
+    marketCap: "55",
+    "24hVolume": "480"
+  },
+  {
+    id: "28",
+    name: "Griffith Mcdonald",
+    lastPrice: "99",
+    "24hChange": "1.2274956111",
+    marketCap: "435",
+    "24hVolume": "96"
+  },
+  {
+    id: "29",
+    name: "Hyacinth Shepard",
+    lastPrice: "312",
+    "24hChange": "1.0778716942",
+    marketCap: "104",
+    "24hVolume": "44"
+  },
+  {
+    id: "30",
+    name: "Kyle Lyons",
+    lastPrice: "387",
+    "24hChange": "0.9088619479",
+    marketCap: "38",
+    "24hVolume": "122"
+  },
+  {
+    id: "31",
+    name: "Adam Travis",
+    lastPrice: "210",
+    "24hChange": "0.6885351785",
+    marketCap: "409",
+    "24hVolume": "285"
+  },
+  {
+    id: "32",
+    name: "Remedios Murphy",
+    lastPrice: "84",
+    "24hChange": "0.2681656625",
+    marketCap: "58",
+    "24hVolume": "56"
+  },
+  {
+    id: "33",
+    name: "Paloma Wilder",
+    lastPrice: "114",
+    "24hChange": "1.1876891304",
+    marketCap: "370",
+    "24hVolume": "479"
+  },
+  {
+    id: "34",
+    name: "Coby Castillo",
+    lastPrice: "97",
+    "24hChange": "1.3222363353",
+    marketCap: "220",
+    "24hVolume": "409"
+  },
+  {
+    id: "35",
+    name: "Cheryl Reeves",
+    lastPrice: "442",
+    "24hChange": "0.5463965424",
+    marketCap: "237",
+    "24hVolume": "239"
+  },
+  {
+    id: "36",
+    name: "Jerome Nguyen",
+    lastPrice: "143",
+    "24hChange": "0.6788507590",
+    marketCap: "201",
+    "24hVolume": "309"
+  },
+  {
+    id: "37",
+    name: "Avye Joyce",
+    lastPrice: "287",
+    "24hChange": "0.8028189650",
+    marketCap: "355",
+    "24hVolume": "144"
+  },
+  {
+    id: "38",
+    name: "Tiger French",
+    lastPrice: "221",
+    "24hChange": "0.5907162217",
+    marketCap: "22",
+    "24hVolume": "51"
+  },
+  {
+    id: "39",
+    name: "Porter Hahn",
+    lastPrice: "355",
+    "24hChange": "0.7618331355",
+    marketCap: "269",
+    "24hVolume": "346"
+  },
+  {
+    id: "40",
+    name: "Byron Wilson",
+    lastPrice: "383",
+    "24hChange": "0.8157165886",
+    marketCap: "7",
+    "24hVolume": "18"
+  },
+  {
+    id: "41",
+    name: "Baxter Glass",
+    lastPrice: "368",
+    "24hChange": "1.2500195481",
+    marketCap: "192",
+    "24hVolume": "284"
+  },
+  {
+    id: "42",
+    name: "Callum Mccoy",
+    lastPrice: "202",
+    "24hChange": "1.2451984606",
+    marketCap: "28",
+    "24hVolume": "220"
+  },
+  {
+    id: "43",
+    name: "Bo Ellison",
+    lastPrice: "281",
+    "24hChange": "0.8498329699",
+    marketCap: "446",
+    "24hVolume": "216"
+  },
+  {
+    id: "44",
+    name: "Cairo Eaton",
+    lastPrice: "202",
+    "24hChange": "0.9312975207",
+    marketCap: "321",
+    "24hVolume": "336"
+  },
+  {
+    id: "45",
+    name: "Paula Swanson",
+    lastPrice: "173",
+    "24hChange": "1.6637069790",
+    marketCap: "6",
+    "24hVolume": "424"
+  },
+  {
+    id: "46",
+    name: "Chastity Strickland",
+    lastPrice: "234",
+    "24hChange": "1.4140179410",
+    marketCap: "182",
+    "24hVolume": "129"
+  },
+  {
+    id: "47",
+    name: "Garth Hendricks",
+    lastPrice: "426",
+    "24hChange": "0.6794679258",
+    marketCap: "439",
+    "24hVolume": "24"
+  },
+  {
+    id: "48",
+    name: "Rana Owen",
+    lastPrice: "197",
+    "24hChange": "0.7224170527",
+    marketCap: "343",
+    "24hVolume": "169"
+  },
+  {
+    id: "49",
+    name: "Zeus Love",
+    lastPrice: "224",
+    "24hChange": "0.8563989506",
+    marketCap: "217",
+    "24hVolume": "174"
+  },
+  {
+    id: "50",
+    name: "Clayton Lester",
+    lastPrice: "272",
+    "24hChange": "0.9085405715",
+    marketCap: "65",
+    "24hVolume": "5"
+  },
+  {
+    id: "51",
+    name: "Valentine William",
+    lastPrice: "470",
+    "24hChange": "1.2172377202",
+    marketCap: "272",
+    "24hVolume": "91"
+  },
+  {
+    id: "52",
+    name: "Alika Young",
+    lastPrice: "208",
+    "24hChange": "1.1127694061",
+    marketCap: "169",
+    "24hVolume": "331"
+  },
+  {
+    id: "53",
+    name: "Dominic Jennings",
+    lastPrice: "226",
+    "24hChange": "0.7846480057",
+    marketCap: "266",
+    "24hVolume": "127"
+  },
+  {
+    id: "54",
+    name: "Elijah Dean",
+    lastPrice: "346",
+    "24hChange": "0.9530035011",
+    marketCap: "393",
+    "24hVolume": "300"
+  },
+  {
+    id: "55",
+    name: "Blythe Strong",
+    lastPrice: "61",
+    "24hChange": "0.8288240799",
+    marketCap: "104",
+    "24hVolume": "413"
+  },
+  {
+    id: "56",
+    name: "Ciara Hanson",
+    lastPrice: "105",
+    "24hChange": "1.0289187800",
+    marketCap: "443",
+    "24hVolume": "259"
+  },
+  {
+    id: "57",
+    name: "Talon Gordon",
+    lastPrice: "357",
+    "24hChange": "1.0844705181",
+    marketCap: "146",
+    "24hVolume": "171"
+  },
+  {
+    id: "58",
+    name: "Yasir Vincent",
+    lastPrice: "286",
+    "24hChange": "1.0360295198",
+    marketCap: "241",
+    "24hVolume": "281"
+  },
+  {
+    id: "59",
+    name: "Neville Gordon",
+    lastPrice: "445",
+    "24hChange": "1.4508133561",
+    marketCap: "326",
+    "24hVolume": "16"
+  },
+  {
+    id: "60",
+    name: "Heather Wilkerson",
+    lastPrice: "315",
+    "24hChange": "1.2096827117",
+    marketCap: "107",
+    "24hVolume": "79"
+  },
+  {
+    id: "61",
+    name: "Mary Moses",
+    lastPrice: "429",
+    "24hChange": "1.2726994864",
+    marketCap: "170",
+    "24hVolume": "245"
+  },
+  {
+    id: "62",
+    name: "Emery Stone",
+    lastPrice: "265",
+    "24hChange": "0.6282186875",
+    marketCap: "146",
+    "24hVolume": "462"
+  },
+  {
+    id: "63",
+    name: "Jacqueline Larsen",
+    lastPrice: "18",
+    "24hChange": "1.0317789798",
+    marketCap: "21",
+    "24hVolume": "379"
+  },
+  {
+    id: "64",
+    name: "Price Fowler",
+    lastPrice: "105",
+    "24hChange": "1.0327242794",
+    marketCap: "436",
+    "24hVolume": "202"
+  },
+  {
+    id: "65",
+    name: "Arthur Douglas",
+    lastPrice: "465",
+    "24hChange": "0.9992046373",
+    marketCap: "372",
+    "24hVolume": "141"
+  },
+  {
+    id: "66",
+    name: "Drake Kerr",
+    lastPrice: "178",
+    "24hChange": "0.8170333736",
+    marketCap: "27",
+    "24hVolume": "492"
+  },
+  {
+    id: "67",
+    name: "Ulysses Kim",
+    lastPrice: "77",
+    "24hChange": "1.0895973997",
+    marketCap: "109",
+    "24hVolume": "75"
+  },
+  {
+    id: "68",
+    name: "Hedda Frye",
+    lastPrice: "234",
+    "24hChange": "0.7186716579",
+    marketCap: "84",
+    "24hVolume": "326"
+  },
+  {
+    id: "69",
+    name: "Ivor Buckner",
+    lastPrice: "343",
+    "24hChange": "1.0107579286",
+    marketCap: "403",
+    "24hVolume": "201"
+  },
+  {
+    id: "70",
+    name: "Edward Johns",
+    lastPrice: "202",
+    "24hChange": "1.1534317607",
+    marketCap: "132",
+    "24hVolume": "222"
+  },
+  {
+    id: "71",
+    name: "Doris Nguyen",
+    lastPrice: "226",
+    "24hChange": "1.3020127915",
+    marketCap: "360",
+    "24hVolume": "94"
+  },
+  {
+    id: "72",
+    name: "Jada Farrell",
+    lastPrice: "436",
+    "24hChange": "1.3376999502",
+    marketCap: "153",
+    "24hVolume": "105"
+  },
+  {
+    id: "73",
+    name: "Hilda Davidson",
+    lastPrice: "12",
+    "24hChange": "1.3050621145",
+    marketCap: "33",
+    "24hVolume": "4"
+  },
+  {
+    id: "74",
+    name: "Wallace Schneider",
+    lastPrice: "304",
+    "24hChange": "1.0719097790",
+    marketCap: "257",
+    "24hVolume": "66"
+  },
+  {
+    id: "75",
+    name: "Macy Cantu",
+    lastPrice: "295",
+    "24hChange": "1.0165995222",
+    marketCap: "270",
+    "24hVolume": "451"
+  },
+  {
+    id: "76",
+    name: "Sasha George",
+    lastPrice: "270",
+    "24hChange": "0.9436667179",
+    marketCap: "415",
+    "24hVolume": "420"
+  },
+  {
+    id: "77",
+    name: "Tobias Robbins",
+    lastPrice: "135",
+    "24hChange": "0.8657128306",
+    marketCap: "345",
+    "24hVolume": "291"
+  },
+  {
+    id: "78",
+    name: "Flavia Sloan",
+    lastPrice: "464",
+    "24hChange": "1.2086072132",
+    marketCap: "237",
+    "24hVolume": "44"
+  },
+  {
+    id: "79",
+    name: "Kylie Weeks",
+    lastPrice: "146",
+    "24hChange": "1.0630170485",
+    marketCap: "282",
+    "24hVolume": "30"
+  },
+  {
+    id: "80",
+    name: "Carol Winters",
+    lastPrice: "306",
+    "24hChange": "1.3456309214",
+    marketCap: "424",
+    "24hVolume": "50"
+  },
+  {
+    id: "81",
+    name: "Rhoda Anthony",
+    lastPrice: "427",
+    "24hChange": "1.2596439053",
+    marketCap: "212",
+    "24hVolume": "378"
+  },
+  {
+    id: "82",
+    name: "Leilani Stevenson",
+    lastPrice: "120",
+    "24hChange": "1.2274777499",
+    marketCap: "113",
+    "24hVolume": "424"
+  },
+  {
+    id: "83",
+    name: "Ulla Bird",
+    lastPrice: "258",
+    "24hChange": "1.3648693937",
+    marketCap: "140",
+    "24hVolume": "468"
+  },
+  {
+    id: "84",
+    name: "Barrett Chen",
+    lastPrice: "79",
+    "24hChange": "0.9601833595",
+    marketCap: "405",
+    "24hVolume": "165"
+  },
+  {
+    id: "85",
+    name: "Sade Norman",
+    lastPrice: "226",
+    "24hChange": "1.0785527984",
+    marketCap: "494",
+    "24hVolume": "445"
+  },
+  {
+    id: "86",
+    name: "Barclay Wynn",
+    lastPrice: "264",
+    "24hChange": "1.2416414583",
+    marketCap: "231",
+    "24hVolume": "391"
+  },
+  {
+    id: "87",
+    name: "Vivien Calhoun",
+    lastPrice: "151",
+    "24hChange": "0.6382538255",
+    marketCap: "91",
+    "24hVolume": "88"
+  },
+  {
+    id: "88",
+    name: "Levi Gonzales",
+    lastPrice: "399",
+    "24hChange": "1.0690988834",
+    marketCap: "224",
+    "24hVolume": "428"
+  },
+  {
+    id: "89",
+    name: "Aurelia Steele",
+    lastPrice: "339",
+    "24hChange": "0.9829199151",
+    marketCap: "144",
+    "24hVolume": "387"
+  },
+  {
+    id: "90",
+    name: "Herrod Galloway",
+    lastPrice: "309",
+    "24hChange": "1.0205377820",
+    marketCap: "366",
+    "24hVolume": "224"
+  },
+  {
+    id: "91",
+    name: "Travis Langley",
+    lastPrice: "401",
+    "24hChange": "0.8843985337",
+    marketCap: "200",
+    "24hVolume": "24"
+  },
+  {
+    id: "92",
+    name: "Brent Smith",
+    lastPrice: "119",
+    "24hChange": "0.8736440042",
+    marketCap: "286",
+    "24hVolume": "143"
+  },
+  {
+    id: "93",
+    name: "Ila Armstrong",
+    lastPrice: "105",
+    "24hChange": "0.8441010643",
+    marketCap: "370",
+    "24hVolume": "126"
+  },
+  {
+    id: "94",
+    name: "Arden Britt",
+    lastPrice: "409",
+    "24hChange": "0.9490994048",
+    marketCap: "316",
+    "24hVolume": "392"
+  },
+  {
+    id: "95",
+    name: "Hedley Lindsay",
+    lastPrice: "7",
+    "24hChange": "1.3412447541",
+    marketCap: "355",
+    "24hVolume": "54"
+  },
+  {
+    id: "96",
+    name: "Cassandra Wynn",
+    lastPrice: "309",
+    "24hChange": "1.2041308367",
+    marketCap: "374",
+    "24hVolume": "332"
+  },
+  {
+    id: "97",
+    name: "Lucian Perkins",
+    lastPrice: "248",
+    "24hChange": "0.4087423716",
+    marketCap: "401",
+    "24hVolume": "378"
+  },
+  {
+    id: "98",
+    name: "Teegan Kemp",
+    lastPrice: "358",
+    "24hChange": "0.7878891157",
+    marketCap: "368",
+    "24hVolume": "421"
+  },
+  {
+    id: "99",
+    name: "Ronan Davis",
+    lastPrice: "148",
+    "24hChange": "1.6463864558",
+    marketCap: "87",
+    "24hVolume": "143"
+  },
+  {
+    id: "100",
+    name: "Keaton Caldwell",
+    lastPrice: "174",
+    "24hChange": "0.9634341145",
+    marketCap: "246",
+    "24hVolume": "351"
+  }
+];
--- src/layouts/dashboards/me/index.tsx
+++ src/layouts/dashboards/me/index.tsx
@@ -1,33 +1,32 @@
-
 // Material Dashboard 2 PRO React TS examples components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-import React, { useState, useEffect } from 'react'
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import { data } from "./data";
 
-
-const rows: GridRowsProp = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
+const rows: GridRowsProp = data;
 
 const columns: GridColDef[] = [
-  { field: 'col1', headerName: 'Column 1', width: 150 },
-  { field: 'col2', headerName: 'Column 2', width: 150 },
+  { field: "name", headerName: "Name", width: 400, editable: true },
+  { field: "lastPrice", headerName: "Last Price", width: 200, editable: true },
+  { field: "24hChange", headerName: "24h Change", width: 200, editable: true },
+  { field: "marketCap", headerName: "Market Cap", width: 200, editable: true },
+  { field: "24hVolume", headerName: "24h Volume", width: 200, editable: true }
 ];
 
 function Me(): JSX.Element {
-
-
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <div style={{ height: 350, width: '100%' }}>
-      <DataGrid columns={columns} rows={rows} />
-    </div>
-      
+      <div style={{ height: 350, width: "100%" }}>
+        <DataGrid
+          columns={columns}
+          rows={rows}
+          onCellEditStop={(grid) => console.log(grid.value)}
+        />
+      </div>
+
       <Footer />
     </DashboardLayout>
   );
